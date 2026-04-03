19bccfab97f62cb49695276a78d62902851a6e8b
Michele Peresano <michele.peresano@to.infn.it>
2026-04-03 11:41:02 +0200

    play with plot

--- src/scripts/random_numbers.py
+++ src/scripts/random_numbers.py
@@ -3,11 +3,12 @@ import numpy as np
 import paths
 
 # Generate some data
-random_numbers = np.random.randn(100, 10)
+random_numbers = np.random.randn(100, 1)
 
 # Plot and save
 fig = plt.figure(figsize=(7, 6))
-plt.plot(random_numbers)
+x = np.linspace(0, 2 * np.pi, 100)
+plt.plot(x, np.sin(x) + random_numbers)
 plt.xlabel("x")
 plt.ylabel("y")
 fig.savefig(paths.figures / "random_numbers.pdf", bbox_inches="tight", dpi=300)
